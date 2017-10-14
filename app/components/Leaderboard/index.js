5eb126b1f20d6e9412e445bb147eaaf16a39df3e
Joseph Kato <joseph@jdkato.io>
2017-10-14 11:46:03 -0700

    feat: implement Leaderboard component

--- app/components/Leaderboard/index.js
+++ app/components/Leaderboard/index.js
@@ -1,23 +1,72 @@
 import React from 'react';
+import styled from 'styled-components';
+import { Button, ButtonGroup } from 'reactstrap';
 import { BootstrapTable, TableHeaderColumn } from 'react-bootstrap-table';
 import axios from 'axios';
+import { FormattedMessage } from 'react-intl';
+
+import messages from './messages';
+
+const StyledTable = styled(BootstrapTable) `
+  padding-top: 20px;
+`;
 
 class Leaderboard extends React.Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
 
     this.state = {
+      active: [],
       overall: [],
       men: [],
       women: [],
+      cSelected: [],
+      rSelected: 1,
     };
+
+    this.onRadioBtnClick = this.onRadioBtnClick.bind(this);
+    this.onCheckboxBtnClick = this.onCheckboxBtnClick.bind(this);
   }
 
-  componentDidMount() {
+  componentWillMount() {
     axios.get('http://localhost:9000/v1/leaderboard/overall')
       .then((res) => {
         this.setState({ overall: this.fixNames(res.data.data) });
+        this.setState({ active: this.state.overall });
       });
+  }
+
+  componentDidMount() {
+    axios.get('http://localhost:9000/v1/leaderboard/men')
+      .then((res) => {
+        this.setState({ men: this.fixNames(res.data.data) });
+      });
+
+    axios.get('http://localhost:9000/v1/leaderboard/women')
+      .then((res) => {
+        this.setState({ women: this.fixNames(res.data.data) });
+      });
+  }
+
+  onRadioBtnClick(rSelected) {
+    this.setState({ rSelected });
+    if (rSelected === 1) {
+      this.setState({ active: this.state.overall });
+    } else if (rSelected === 2) {
+      this.setState({ active: this.state.men });
+    } else {
+      this.setState({ active: this.state.women });
+    }
+  }
+
+  onCheckboxBtnClick(selected) {
+    const index = this.state.cSelected.indexOf(selected);
+    if (index < 0) {
+      this.state.cSelected.push(selected);
+    } else {
+      this.state.cSelected.splice(index, 1);
+    }
+    this.setState({ cSelected: [...this.state.cSelected] });
   }
 
   /* eslint-disable no-param-reassign */
@@ -31,13 +80,36 @@ class Leaderboard extends React.Component {
 
   render() {
     return (
-      <BootstrapTable data={this.state.overall} striped hover>
-        <TableHeaderColumn isKey dataField="Name">Name</TableHeaderColumn>
-        <TableHeaderColumn dataField="Speed">Speed</TableHeaderColumn>
-        <TableHeaderColumn dataField="Consistency">Consistency</TableHeaderColumn>
-        <TableHeaderColumn dataField="Success">Success</TableHeaderColumn>
-        <TableHeaderColumn dataField="Rating">Ninja Rating</TableHeaderColumn>
-      </BootstrapTable>
+      <div>
+        <ButtonGroup>
+          <Button color="primary" onClick={() => this.onRadioBtnClick(1)} active={this.state.rSelected === 1}>
+            <FormattedMessage {...messages.overall} />
+          </Button>
+          <Button color="primary" onClick={() => this.onRadioBtnClick(2)} active={this.state.rSelected === 2}>
+            <FormattedMessage {...messages.men} />
+          </Button>
+          <Button color="primary" onClick={() => this.onRadioBtnClick(3)} active={this.state.rSelected === 3}>
+            <FormattedMessage {...messages.women} />
+          </Button>
+        </ButtonGroup>
+        <StyledTable data={this.state.active} striped hover>
+          <TableHeaderColumn isKey dataField="Name">
+            <FormattedMessage {...messages.name} />
+          </TableHeaderColumn>
+          <TableHeaderColumn dataField="Speed">
+            <FormattedMessage {...messages.speed} />
+          </TableHeaderColumn>
+          <TableHeaderColumn dataField="Consistency">
+            <FormattedMessage {...messages.consistency} />
+          </TableHeaderColumn>
+          <TableHeaderColumn dataField="Success">
+            <FormattedMessage {...messages.success} />
+          </TableHeaderColumn>
+          <TableHeaderColumn dataField="Rating">
+            <FormattedMessage {...messages.rating} />
+          </TableHeaderColumn>
+        </StyledTable>
+      </div>
     );
   }
 }
---
+++ app/components/Leaderboard/messages.js
@@ -0,0 +1,41 @@
+/*
+ * Leaderboard Messages
+ *
+ * This contains all the text for the Leaderboard component.
+ */
+import { defineMessages } from 'react-intl';
+
+export default defineMessages({
+  overall: {
+    id: 'ninjaref.components.Leaderboard.overall',
+    defaultMessage: 'Overall',
+  },
+  men: {
+    id: 'ninjaref.components.Leaderboard.men',
+    defaultMessage: 'Men',
+  },
+  women: {
+    id: 'ninjaref.components.Leaderboard.women',
+    defaultMessage: 'Women',
+  },
+  speed: {
+    id: 'ninjaref.components.Leaderboard.speed',
+    defaultMessage: 'Speed',
+  },
+  consistency: {
+    id: 'ninjaref.components.Leaderboard.consistency',
+    defaultMessage: 'Consistency',
+  },
+  success: {
+    id: 'ninjaref.components.Leaderboard.success',
+    defaultMessage: 'Success',
+  },
+  rating: {
+    id: 'ninjaref.components.Leaderboard.rating',
+    defaultMessage: 'Ninja Rating',
+  },
+  name: {
+    id: 'ninjaref.components.Leaderboard.name',
+    defaultMessage: 'Name',
+  },
+});
